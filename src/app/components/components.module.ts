c080013efdb1b1490da8aeead3343be48b37e134
Liberty Failor <Libertyjayne@gmail.com>
2020-08-20 19:39:24 -0500

    banner comp

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
 // import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BannerComponent],
   // declarations: [AboutComponent]
   imports: [
     CommonModule
